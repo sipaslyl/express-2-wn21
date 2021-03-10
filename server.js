cd2e1309cffe1dca6029c6040ac8d7887e0deb7e
Lylla <72465355+sipaslyl@users.noreply.github.com>
2021-03-10 15:04:31 -0800

    Adds data to server.js and a console log command

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var express = require('express');
-
+var data = require('./data/test.json');
 var app = express();
 const PORT = 3000;
 
@@ -13,4 +13,5 @@ app.get('/', (req,res)=>{
 
 app.listen(PORT,()=>{
   console.log(`App is running on port ${PORT}`);
+  console.log(data)
 });
